f8139e30f7e7b97edeec3b5c2e7e12fb428bc7ce
Tripathy <abhi99@iastate.edu>
2019-12-11 14:01:43 -0600

    fwd_unitDF works

--- ProjC/fwd_Unit/fwd_unitDF.vhd
+++ ProjC/fwd_Unit/fwd_unitDF.vhd
@@ -6,23 +6,34 @@ entity fwd_unit is
 		WB_rd  : in std_logic_vector (4 downto 0);
 		MEM_regWr : in std_logic;
 		WB_regWr  : in std_logic;
-		read1_in  : in std_logic_vector (4 downto 0);
-		read2_in  : in std_logic_vector (4 downto 0);
-		fwd_out1  : out std_logic_vector (1 downto 0);
-		fwd_out2  : out std_logic_vector (1 downto 0));
+		in_EX_rs  : in std_logic_vector (4 downto 0);	--(EX.rs)
+		in_EX_rt  : in std_logic_vector (4 downto 0);   --(EX.rt)
+		fwd_out1  : out std_logic_vector (1 downto 0);	--1stmux (ALU in A)
+		fwd_out2  : out std_logic_vector (1 downto 0));	--2ndmux (ALU in A)
 
 end fwd_unit;
 
 architecture dataflow of fwd_unit is
 
 begin
-if (MEM_regWr=1) AND (WB_regWr=0) AND (MEM_rd=read1_in) then fwd_out1="10";
-else fwd_out1="00";
-endif;
-if (MEM_regWr=0) AND (WB_regWr=1) AND (WB_rd=read1_in) then fwd_out1="01";
-else fwd_out1="00";
-endif;
-if (MEM_regWr=1) AND (WB_regWr=1) then
-	 if (WB_rd=read1_in)
+	process(MEM_rd, WB_rd, MEM_regWr, WB_regWr, in_EX_rs, in_EX_rt)
+	begin
+		if    (MEM_regWr='1') and (WB_regWr='0') and (MEM_rd=in_EX_rs) then fwd_out1<="10";
+		elsif (MEM_regWr='0') and (WB_regWr='1') and (WB_rd=in_EX_rs) then fwd_out1<="01";
+		elsif (MEM_regWr='1') and (WB_regWr='1') and (MEM_rd=in_EX_rs) and (WB_rd/=in_EX_rs)  then fwd_out1<="10";
+		elsif (MEM_regWr='1') and (WB_regWr='1') and (MEM_rd/=in_EX_rs) and (WB_rd=in_EX_rs)  then fwd_out1<="01";
+		elsif (MEM_regWr='1') and (WB_regWr='1') and (MEM_rd=in_EX_rs) and (WB_rd=in_EX_rs)  then fwd_out1<="10";
+		else fwd_out1<="00";
+		end if;
+
+		if (MEM_regWr='1') and (WB_regWr='0') and (MEM_rd=in_EX_rt) then fwd_out2<="10";
+		elsif (MEM_regWr='0') and (WB_regWr='1') and (WB_rd=in_EX_rt) then fwd_out2<="01";	
+		elsif (MEM_regWr='1') and (WB_regWr='1') and (MEM_rd=in_EX_rt) and (WB_rd/=in_EX_rt)  then fwd_out2<="10";	
+		elsif (MEM_regWr='1') and (WB_regWr='1') and (MEM_rd/=in_EX_rt) and (WB_rd=in_EX_rt)  then fwd_out2<="01";
+		elsif (MEM_regWr='1') and (WB_regWr='1') and (MEM_rd=in_EX_rt) and (WB_rd=in_EX_rt)  then fwd_out2<="10";
+		else fwd_out2<="00";
+		end if;
+
+	end process;
 	
 end dataflow;
