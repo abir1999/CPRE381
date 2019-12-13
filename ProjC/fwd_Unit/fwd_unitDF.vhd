3c440b07277fea0d24b59b92fcc925929d1cab96
Mojumder <abir99@iastate.edu>
2019-12-13 15:36:06 -0600

    new fwd

--- ProjC/fwd_Unit/fwd_unitDF.vhd
+++ ProjC/fwd_Unit/fwd_unitDF.vhd
@@ -2,8 +2,8 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 --Jump and Branch address is calculated within ID stage, this requirement needs to be changed for Proj part B implementation.
 entity fwd_unit is
-	port(	MEM_rd : in std_logic_vector (4 downto 0);
-		WB_rd  : in std_logic_vector (4 downto 0);
+	port(	
+		in_WBRegAddr  : in std_logic_vector (4 downto 0);
 		MEM_regWr : in std_logic;
 		WB_regWr  : in std_logic;
 		in_MEMRegAddr : in std_logic_vector (4 downto 0);
@@ -20,28 +20,29 @@ end fwd_unit;
 architecture dataflow of fwd_unit is
 
 begin
-	process(MEM_rd, WB_rd, MEM_regWr, WB_regWr, in_EX_rs, in_EX_rt)
+	process(in_MEMRegAddr, in_WBRegAddr, MEM_regWr, WB_regWr, in_EX_rs, in_EX_rt)
 	begin
-		if    (MEM_regWr='1') and (WB_regWr='0') and (MEM_rd=in_EX_rs) then fwd_outA<="10";
-		elsif (MEM_regWr='0') and (WB_regWr='1') and (WB_rd=in_EX_rs) then fwd_outA<="01";
-		elsif (MEM_regWr='1') and (WB_regWr='1') and (MEM_rd=in_EX_rs) and (WB_rd/=in_EX_rs)  then fwd_outA<="10";
-		elsif (MEM_regWr='1') and (WB_regWr='1') and (MEM_rd/=in_EX_rs) and (WB_rd=in_EX_rs)  then fwd_outA<="01";
-		elsif (MEM_regWr='1') and (WB_regWr='1') and (MEM_rd=in_EX_rs) and (WB_rd=in_EX_rs)  then fwd_outA<="10";
+		if    (MEM_regWr='1') and (WB_regWr='0') and (in_MEMRegAddr=in_EX_rs) then fwd_outA<="10";
+		elsif (MEM_regWr='0') and (WB_regWr='1') and (in_WBRegAddr=in_EX_rs) then fwd_outA<="01";
+		elsif (MEM_regWr='1') and (WB_regWr='1') and (in_MEMRegAddr=in_EX_rs) and (in_WBRegAddr/=in_EX_rs)  then fwd_outA<="10";
+		elsif (MEM_regWr='1') and (WB_regWr='1') and (in_MEMRegAddr/=in_EX_rs) and (in_WBRegAddr=in_EX_rs)  then fwd_outA<="01";
+		elsif (MEM_regWr='1') and (WB_regWr='1') and (in_MEMRegAddr=in_EX_rs) and (in_WBRegAddr=in_EX_rs)  then fwd_outA<="10";
 		else fwd_outA<="00";
 		end if;
 
-		if (MEM_regWr='1') and (WB_regWr='0') and (MEM_rd=in_EX_rt) then fwd_outB<="10";
-		elsif (MEM_regWr='0') and (WB_regWr='1') and (WB_rd=in_EX_rt) then fwd_outB<="01";	
-		elsif (MEM_regWr='1') and (WB_regWr='1') and (MEM_rd=in_EX_rt) and (WB_rd/=in_EX_rt)  then fwd_outB<="10";	
-		elsif (MEM_regWr='1') and (WB_regWr='1') and (MEM_rd/=in_EX_rt) and (WB_rd=in_EX_rt)  then fwd_outB<="01";
-		elsif (MEM_regWr='1') and (WB_regWr='1') and (MEM_rd=in_EX_rt) and (WB_rd=in_EX_rt)  then fwd_outB<="10";
+		if (MEM_regWr='1') and (WB_regWr='0') and (in_MEMRegAddr=in_EX_rt) then fwd_outB<="10";
+		elsif (MEM_regWr='0') and (WB_regWr='1') and (in_WBRegAddr=in_EX_rt) then fwd_outB<="01";	
+		elsif (MEM_regWr='1') and (WB_regWr='1') and (in_MEMRegAddr=in_EX_rt) and (in_WBRegAddr/=in_EX_rt)  then fwd_outB<="10";	
+		elsif (MEM_regWr='1') and (WB_regWr='1') and (in_MEMRegAddr/=in_EX_rt) and (in_WBRegAddr=in_EX_rt)  then fwd_outB<="01";
+		elsif (MEM_regWr='1') and (WB_regWr='1') and (in_MEMRegAddr=in_EX_rt) and (in_WBRegAddr=in_EX_rt)  then fwd_outB<="10";
 		else fwd_outB<="00";
 		end if;
 		
-		if (MEM_rd="00000") then fwd_outA<="00";
-		if (WB_rd="00000") then fwd_outB<="00";
-		if(in_ID_rs = in_MEMRegAddr) then fwd_branchA <= '1';	
-		elsif (in_ID_rt = in_MEMRegAddr) then fwd_branchB <= '1';
+		if (in_EX_rs="00000") then fwd_outA<="00";
+		if (in_EX_rt="00000") then fwd_outB<="00";
+		
+		if(in_ID_rs = in_WBRegAddr) then fwd_branchA <= '1';	
+		elsif (in_ID_rt = in_WBRegAddr) then fwd_branchB <= '1';
 		else 
 		fwd_branchA <='0';
 		fwd_branchB <= '0';
